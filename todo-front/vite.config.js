75156ebba09dd37f3f4cc1648f83c7f438e9da1a
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-06-11 23:58:16 -0300

    update: viteproxy

--- todo-front/vite.config.js
+++ todo-front/vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
         target: 'http://3.128.200.151:80',
         changeOrigin: true,
         secure: false,
+        // rewrite: path => path.replace(/^\/api/, '') // use isso se o backend não espera o prefixo /api
       }
     }
   }
