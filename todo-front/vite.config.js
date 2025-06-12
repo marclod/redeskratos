b1655c3f00d6dc6af81e8968bf1c98b95f3fc20e
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-06-11 23:54:52 -0300

    update: vite.config proxy

--- todo-front/vite.config.js
+++ todo-front/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://3.128.200.151:80',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
